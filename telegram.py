7cacbad678719378ae66534e36b13ae4528ec921
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2023-02-09 01:27:57 +0500

    Update telegram notification agent to use the new method
    Telegram notification agent would now read the contents off of tvshows.txt and movies.txt and merge them together before sending them to telegram

--- telegram.py
+++ telegram.py
@@ -19,17 +19,25 @@ def send_to_telegram(text):
     if response.status_code != 200:
         raise Exception(f'Error sending message to Telegram: {response.content}')
 
-def send_output_to_telegram():
-    with open('output.txt', 'r') as file:
-        text = file.read()
-        if text:
-            send_to_telegram(text)
+def send_merged_to_telegram():
+    merged_text = ""
+    with open('tvshows.txt', 'r') as file:
+        tvshows = file.read()
+        if tvshows:
+            merged_text += tvshows
         file.close()
-    with open('output.txt', 'w') as file:
-        file.write('')
+
+    with open('movies.txt', 'r') as file:
+        movies = file.read()
+        if movies:
+            merged_text += movies
+        file.close()
+
+    if merged_text:
+        send_to_telegram(merged_text)
 
 if __name__ == "__main__":
-    schedule.every(1).hour.do(send_output_to_telegram)
+    schedule.every(6).hours.do(send_merged_to_telegram)
     while True:
         schedule.run_pending()
         time.sleep(1)
