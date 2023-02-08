b91b9569b35374c968582d3b099d46da010f35ee
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2023-02-08 22:33:14 +0500

    Fix the movies test script

--- movietest.sh
+++ movietest.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -H "Content-Type: application/json" -d '{"title": "Incesptio
-n", "movies": "A mind-bending thriller about a talented extractor who is off
-ered a chance to regain his old life as payment for a task considered to be
-impossible: Inception, the implantation of another persons idea into a targe
-ts subconscious"}' http://localhost:5000/webhook
+curl -X POST -H "Content-Type: application/json" -d '{"title": "Inception", "movies": "A mind-bending thriller about a talented extractor who is offered a chance to regain his old life as payment for a task considered to be impossible: Inception, the implantation of another persons idea into a targets subconscious"}' http://localhost:5000/webhook
