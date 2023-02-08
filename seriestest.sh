1ac316e195a7b2d3d0eec24b55969cd1bb33f41f
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2023-02-08 10:04:43 +0500

    rename test.sh to seriestest.sh

--- seriestest.sh
+++ seriestest.sh
@@ -4,9 +4,9 @@ URL="http://localhost:5000/webhook"
 
 for season in {1..2}; do
   for episode in $(seq -f "E%02g" 1 10); do
-    data=$(echo -n '{"title": "TV SHOW", "episodes": "S0'$season$episode'"}')
+    data=$(echo -n '{"title": "My Mum", "episodes": "S0'$season$episode'"}')
     curl -H "Content-Type: application/json" -X POST -d "$data" "$URL"
-    echo '{"title": "TV SHOW", "episode": "S0'$season$episode'"}'
+    echo '{"title": "My mom", "episode": "S0'$season$episode'"}'
   done
   echo
 done
