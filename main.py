8db6cdf365f1ccfc5a5b18c604fd31921c97be4f
Fallenbagel <98979876+Fallenbagel@users.noreply.github.com>
2023-02-08 10:02:25 +0500

    Add initial movie notification support
    note: this is broken. Movie support works but after a movie notification has been sent, no other notifications will work

--- main.py
+++ main.py
@@ -7,8 +7,13 @@ app = Flask(__name__)
 def webhook():
     json_data = request.get_json()
     title = json_data.get("title")
-    episodes = json_data.get("episodes")
-
+    episode = json_data.get("episodes")
+    if episode:
+      season = episode.split("E")[0]
+      episode_number = int(episode.split("E")[1])
+    else:
+      movie = json_data.get("movies")
+      
     try:
         with open("output.txt", "r") as file:
             existing_contents = file.read()
@@ -16,18 +21,43 @@ def webhook():
         existing_contents = ""
 
     existing_titles = existing_contents.split("\n\n")
-    existing_titles = [x.split("\n") for x in existing_titles if x]
-    existing_titles = {x[0]: x[1:] for x in existing_titles}
+    existing_titles = [x for x in existing_titles if x]
+    existing_titles = [[x.split("\n")[0], *x.split("\n")[1:]] for x in existing_titles]
+    existing_titles = {x[0]: {y.split("-")[0].split("E")[0]: {'start': int(y.split("-")[0].split("E")[1]), 'end': int(y.split("-")[1].split("E")[1])} for y in x[1:]} for x in existing_titles}
 
     if title in existing_titles:
-        existing_titles[title].append(episodes)
+        content = existing_titles[title]
+        if "movies" in content:
+            existing_titles[title] = {}
+        else:
+            season = episode.split("E")[0]
+            episode_number = int(episode.split("E")[1])
+        if season not in existing_titles[title]:
+            existing_titles[title][season] = {'start': episode_number, 'end': episode_number}
+        else:
+            existing_start = existing_titles[title][season]['start']
+            existing_end = existing_titles[title][season]['end']
+            existing_titles[title][season]['start'] = min(existing_start, episode_number)
+            existing_titles[title][season]['end'] = max(existing_end, episode_number)
     else:
-        existing_titles[title] = [episodes]
+        existing_titles[title] = {}
+        if episode is None:
+            existing_titles[title]["movies"] = json_data.get("movies")
+        else:
+            season = episode.split("E")[0]
+            episode_number = int(episode.split("E")[1])
+            existing_titles[title][season] = {'start': episode_number, 'end': episode_number}
+
 
     with open("output.txt", "w") as file:
-        for title, episodes in existing_titles.items():
+        for title, content in existing_titles.items():
             file.write(f"{title}\n")
-            file.write(", ".join(episodes) + "\n\n")
+            if "movies" in content:
+                file.write(f"{content['movies']}\n\n")
+            else:
+                for season, episodes in content.items():
+                    file.write(f"{season}E{str(episodes['start']).zfill(2)} - {season}E{str(episodes['end']).zfill(2)}\n")
+                file.write("\n")
 
     return "OK"
 
