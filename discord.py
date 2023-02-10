b1aa9d5102dcfd609a4ac9326f9d424c309166c8
Joe Harrison <joe@j-harrison.co.uk>
2023-02-10 08:27:57 +0000

    .

--- discord.py
+++ discord.py
@@ -77,11 +77,11 @@ if __name__ == "__main__":
 
     # Schedule the send_merged_to_discord function
     if schedule_unit == 's':
-        schedule.every(schedule_number).seconds.do(send_merged_to_discord, args.url, args.authorization)
+        schedule.every(schedule_number).seconds.do(send_merged_to_discord, args.token, args.channelid)
     elif schedule_unit == 'm':
-        schedule.every(schedule_number).minutes.do(send_merged_to_discord, args.url, args.authorization)
+        schedule.every(schedule_number).minutes.do(send_merged_to_discord, args.token, args.channelid)
     elif schedule_unit == 'h':
-        schedule.every(schedule_number).hours.do(send_merged_to_discord, args.url, args.authorization)
+        schedule.every(schedule_number).hours.do(send_merged_to_discord, args.token, args.channelid)
     else:
         raise Exception('Invalid schedule unit, should be either "s" for seconds, "m" for minutes, or "h" for hours')
 
